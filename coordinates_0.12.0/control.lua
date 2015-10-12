b91014422e908cf578c369410ddfd00f1d4f4f85
Gergith <gergith+github@gmail.com>
2015-10-12 19:50:13 -0400

    Factorio Coordinates Mod 0.12.0
    Factorio Coordinates Mod 0.12.0
    
    First version of mod to show current player coordinates/position.

---
+++ coordinates_0.12.0/control.lua
@@ -0,0 +1,11 @@
+require "defines"
+
+if not coordinates then coordinates = {} end
+
+game.on_event(defines.events.on_tick, function(event)
+    
+		for i, player in ipairs(game.players) do
+			if player.gui.top.coordinatesGUI == nil then player.gui.top.add{type="button", name="coordinatesGUI"} end
+			player.gui.top.coordinatesGUI.caption=string.format("x=%f y=%f",game.player.character.position.x,game.player.character.position.y)
+		end
+end)
