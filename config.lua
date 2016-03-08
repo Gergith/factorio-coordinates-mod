00b5f69b9b21129824393b1297fefd596b5c8847
Gergith <gergith+github@gmail.com>
2016-03-07 22:24:10 -0500

    1.2.0
    - Added ability to show Z Coordinates (String instead of an int, to
    allow for mods)
    - Added option in config file to enable/disable Z Coordinates (disabled
    by default)
    - Changed default config for significant digits to 0
    - Fixed MAJOR bug with the significant digits changing with ANY button
    click, regardless of which plugin.

--- config.lua
+++ config.lua
@@ -1,2 +1,4 @@
--- defaults to 4. If higher than 4, will round down. If lower than 0, will round up.
-coordinates.significantdigits = 4
+-- Changes amount of decimal places shown on the mod. defaults to 0. If higher than 4, will round down. If lower than 0, will round up.
+coordinates.significantdigits = 0
+-- defaults to false. Allows for z coordinates to show up.
+coordinates.showz = false
--- control.lua
+++ control.lua
@@ -2,22 +2,26 @@ require "defines"
 
 if not coordinates then coordinates = {} end
 if not coordinates.significantdigits then coordinates.significantdigits = {} end
+if not coordinates.showz then coordinates.showz = {} end
 
 require "config"
 
 if coordinates.significantdigits > 4 then coordinates.significantdigits = 4 end
 if coordinates.significantdigits < 0 then coordinates.significantdigits = 0 end
+if coordinates.showz == nil then coordinates.showz = false end
 
 script.on_event(defines.events.on_tick, function(event)
-
-		for i, player in ipairs(game.players) do
-			if player.gui.top.coordinatesGUI == nil then player.gui.top.add{type="button", name="coordinatesGUI"} end
-			template = string.format("x=%%.%.df y=%%.%.df", coordinates.significantdigits, coordinates.significantdigits)
-			player.gui.top.coordinatesGUI.caption = string.format(template, game.player.character.position.x,game.player.character.position.y)
+	for i, player in ipairs(game.players) do
+		if player.gui.top.coordinatesGUI == nil then player.gui.top.add{type="button", name="coordinatesGUI"} end
+		if coordinates.showz == true then surfacestring = " z="..player.surface.name else surfacestring=""  end
+		template = string.format("x=%%.%.df y=%%.%.df", coordinates.significantdigits, coordinates.significantdigits)
+		player.gui.top.coordinatesGUI.caption = string.format(template..surfacestring, game.player.character.position.x,game.player.character.position.y)
 		end
-end)
+	end)
 
 script.on_event(defines.events.on_gui_click, function(event)
-	coordinates.significantdigits =  coordinates.significantdigits + 1
-	if coordinates.significantdigits > 4 then coordinates.significantdigits = 0 end
-end)
+	if event.element.name == "coordinatesGUI" then 
+		coordinates.significantdigits =  coordinates.significantdigits + 1
+		if coordinates.significantdigits > 4 then coordinates.significantdigits = 0 end
+    	end
+	end)
